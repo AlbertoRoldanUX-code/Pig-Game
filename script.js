635b2644628602cfbbdbbf5f4c25e9c361051c89
Alberto Roldán <a-f-r@hotmail.es>
2021-03-11 00:22:13 +0100

    Put scores to 0 and make dice disappear

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let scorePlayer1 = document.querySelector('#score--0');
+
+scorePlayer1.textContent = '0';
+
+let scorePlayer2 = document.querySelector('#score--1');
+
+scorePlayer2.textContent = '0';
+
+let dice = document.querySelector('.dice');
+
+dice.style.visibility = 'hidden';
